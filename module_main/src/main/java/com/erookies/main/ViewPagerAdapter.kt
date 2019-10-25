e9efc56b3347fce559b41e91831190d4704a4288
cchanges <changes720@outlook.com>
2019-10-25 13:45:58 +0800

    [Feature | Bugfix] 加入用户页面，修复原来框架不严谨的部分
    修复大小写，添加部分拓展方法，在module中添加Arouter的compiler保证能够解析注解
    添加用户页
    
    Signed-off-by: cchanges <changes720@outlook.com>

---
+++ module_main/src/main/java/com/erookies/main/ViewPagerAdapter.kt
@@ -0,0 +1,6 @@
+package com.erookies.main
+
+/**
+ * Create by Cchanges.
+ * Time: 2019-10-22
+ */
---
+++ module_mine/src/main/java/com/erookies/mine/FakeUser.kt
@@ -0,0 +1,6 @@
+package com.erookies.mine
+
+/**
+ * Create by Cchanges.
+ * Time: 2019-10-25
+ */
